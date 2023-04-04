2d7e2e312a4c7fdb48e44cf6371b43bad7ea1298
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-04-04 14:28:51 +0530

    Added:SortingInArrayForEmployeeWadge.js file

---
+++ SortingInArrayForEmployeeWadge.js
@@ -0,0 +1,47 @@
+const IS_PART_Time = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
+const MAX_Hours = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empWageArray = new Array();
+
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
+    case IS_PART_Time:
+      return PART_TIME_HOURS;
+      break;
+    case IS_FULL_TIME:
+      return FULL_TIME_HOURS;
+      break;
+    default:
+      return 0;
+  }
+}
+
+function calculateDailyWage(empHrs) {
+  return empHrs * WAGE_PER_HOUR;
+}
+
+while (totalEmpHrs <= MAX_Hours && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+  totalWorkingDays++;
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  totalEmpHrs += getWorkingHours(empCheck);
+  let empHrs = getWorkingHours(empCheck);
+  totalEmpHrs += empHrs;
+  empWageArray.push(calculateDailyWage(empHrs));
+}
+
+let employeeWage = totalEmpHrs * WAGE_PER_HOUR;
+let emplWage = calculateDailyWage(totalEmpHrs);
+console.log(
+  'Total working days ' +
+    totalWorkingDays +
+    ' Total emp hrs: ' +
+    totalEmpHrs +
+    ' Emp Wage: ' +
+    empWage
+);
